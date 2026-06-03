d66deb2d6ad7bac83bb05ddefc62ddbfc613dfd9
AY-CODED <ayomiposoyinka@gmail.com>
2026-06-03 22:38:34 +0100

    change

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
 
 import Home from "./pages/Home.jsx";
-import About from "./pages/About.jsx";
+// import About from "./pages/About.jsx";
 // import Courses from "./pages/Courses.jsx";
-import CourseDetails from "./pages/CourseDetails.jsx";
+// import CourseDetails from "./pages/CourseDetails.jsx";
 import Contact from "./pages/Contact.jsx";
 import Enroll from "./pages/Enroll.jsx";
 
